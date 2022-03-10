6dde674d32224e258e4697d701c586a7b86ece20
Dapada <92043688+Dapada@users.noreply.github.com>
2022-03-09 21:38:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const createAccountForm = document.querySelector("#createAccount");
 
 
-  document.querySelector("#linkCreateAccount").addEventListener("click", () => {
+  document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+    e.preventDefault();
     loginForm.classList.add("form--hidden");
     createAccountForm.classList.remove("form--hidden");
   });
 
-  document.querySelector("#linkLogin").addEventListener("click", () => {
+  document.querySelector("#linkLogin").addEventListener("click", e => {
+    e.preventDefault();
     loginForm.classList.remove("form--hidden");
     createAccountForm.classList.add("form--hidden");
   });
